f929c513875bdc8a4f8a192656a86e97d4a1a602
juaneins_uio <ballesterosjg@gmail.com>
2025-08-26 10:17:14 -0500

    Configuracion de reglas de acceso en spring security

--- src/main/java/com/delivery/web/config/SecurityConfig.java
+++ src/main/java/com/delivery/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,13 +15,16 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
+        http	.csrf(AbstractHttpConfigurer::disable)
+        		.cors(withDefaults())    
                 .authorizeHttpRequests(
                         (authorize) -> authorize
-                                .anyRequest()
-                                .authenticated())
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(withDefaults())
+                        .requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll() 
+                        .requestMatchers(HttpMethod.PUT).denyAll()
+                        .anyRequest()
+                        .authenticated()
+                		)                
+                            
                 .httpBasic(withDefaults()); 
 		
 		return http.build();
